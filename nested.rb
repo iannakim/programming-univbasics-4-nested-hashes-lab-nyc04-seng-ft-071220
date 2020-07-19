88d678922d51f761adfa4b1e5343bba297730936
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-19 23:14:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmmer_hash[:yukihiro_matsumoto]
+programmmer_hash[:yukihiro_matsumoto]={:known_for, :languages}
 programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
 programmer_hash
