7047d559d890c797876660dd36e66a33e9f900ee
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-19 23:09:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,9 +60,9 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
+ # :yukihiro_matsumoto => {
+  #  :known_for => "Ruby",
+  # :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
 
@@ -81,7 +81,13 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmmer_hash = {
+  :yukihiro_matsumoto => {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+}
+programmer_hash
 end
 
 def changing_alan
